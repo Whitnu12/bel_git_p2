9d3743fe948244025aac4d3fc3ef092f1bb4fbdb
Whitnu12 <whitnu@gmail.com>
2021-11-21 14:39:56 +0800

    Mencoba Javascript

---
+++ latihanjs.js
@@ -0,0 +1,24 @@
+var age = 50;
+var name = 'whitnu Nastain';
+let date = 20;
+const color = 'cek';
+
+if (age <= 20 && age >= 10) {
+  console.log(`Hallo ${name}`);
+} else {
+  console.log(`bukan ${name}`);
+}
+
+switch (color) {
+  case 'red':
+    console.log('bukan budjang');
+    break;
+
+  case 'green':
+    console.log(`Hallo ${name}`);
+    break;
+
+  default:
+    console.log('bukan siapa siapa');
+    break;
+}
